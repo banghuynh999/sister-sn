b829179a22f365a86922de20825a8071f86f25f6
Equal <82250087+banghuynh999@users.noreply.github.com>
2023-03-22 23:59:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng em gái';
+const contentCard = 'Nhân ngày sinh thần của em. Anh chúc em gái ngày càng xinh xắn, luôn yêu đời, công việc được như ý. Anh thực sự không biết tặng em gì, anh nghĩ thì hiện giờ anh không nên tặng em gì quá là vật chất, dễ hư lém,... Nên anh nghĩ a tạo thiệp chúc như này là hợp lý mong em đừng buồn vì không có quà há <3';
+
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
